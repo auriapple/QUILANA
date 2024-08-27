f2185fc40fb3eca700c7bd31cdb2feb94a14311c
Angelito Mampusti <mampustiangelito@gmail.com>
2024-08-27 17:03:42 +0800

    Changed

--- header.php
+++ header.php
@@ -1,20 +1,39 @@
 <meta charset = "utf-8" />
-		<link rel = "stylesheet" type = "text/css" href = "assets/css/bootstrap.min.css" />
-		<link rel = "stylesheet" type = "text/css" href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-		<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+	<!--
+	<link rel = "stylesheet" type = "text/css" href = "assets/css/bootstrap.min.css" />
+	<link rel = "stylesheet" type = "text/css" href = "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/css/select2.min.css" rel="stylesheet" />
+		
+	<link href="https://fonts.cdnfonts.com/css/svn-gilroy" rel="stylesheet">-->
+
+		<!--  Bootsrap CSS -->
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  	<!--  Bootsrap Icons -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 
 		
 
 	<script src = "assets/js/jquery-3.5.1.min.js"></script>
-	<script src = "assets/js/bootstrap.min.js"></script>
+	<!--  <script src = "assets/js/bootstrap.min.js"></script>-->
+
+	
 	<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script><script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
-<link href="https://fonts.cdnfonts.com/css/svn-gilroy" rel="stylesheet">
+	<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-beta.1/dist/js/select2.min.js"></script>
+	<!--  Jquery -->
+	<script src="assets/js/jquery-3.7.1.min.js"></script>
+	<!-- Bootsrap Bundle -->
+	<script src="assets/js/bootstrap.bundle.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+
 <style>
 	h3,a,div,button{
 		font-family: 'SVN-Gilroy', sans-serif;
 	}
 </style>
+
 <link rel = "stylesheet" type = "text/css" href = "assets/css/style.css" />
 <link rel = "stylesheet" type = "text/css" href = "assets/css/custom-tables.css" />
+<!--  Sidebar/Navbar CSS -->
+<link rel="stylesheet" href="assets/css/navbar.css">
+<!--  Sidebar/Navbar CSS -->
--- nav_bar.php
+++ nav_bar.php
@@ -1,4 +1,109 @@
-			<nav class = "navbar navbar-header navbar-light bg-primary">
+<!-- New Sidebar -->
+<div class="wrapper">
+        <aside id="sidebar">
+            <div class="d-flex">
+                <button class="toggle-btn" type="button">
+                    <i class="bi bi-router-fill"></i>
+                </button>
+                <div class="sidebar-logo">
+                    <a href="#">Quilana</a>
+                </div>
+            </div>
+            <ul class="sidebar-nav">
+                <li class="sidebar-item">
+                    <a href="home.php" class="sidebar-link">
+                        <i class="bi bi-bar-chart-fill"></i>
+                        <span>Dashboard</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="class_list.php" class="sidebar-link">
+                        <i class="bi bi-person-workspace"></i>
+                        <span>Classes</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="#" class="sidebar-link">
+                        <i class="bi bi-pencil-square"></i>
+                        <span>Assessments</span>
+                    </a>
+                </li>
+            </ul>
+            <div class="sidebar-footer">
+                <a href="#" class="sidebar-link">
+										<i class="bi bi-sign-turn-left-fill"></i>
+                    <span>Logout</span>
+                </a>
+            </div>
+        </aside>
+        <div class="main">
+            <nav class="navbar navbar-expand px-4 py-3">
+                <div class="navbar-collapse collapse">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item dropdown">
+                            <a href="#" data-bs-toggle="dropdown" aria-expanded="false" class="nav-icon pe-md-0">
+                                <img src="image/account.png" class="avatar img-fluid" alt="">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end rounded">
+                                <a href="#" class="dropdown-item">
+                                    <i class="bi bi-person-circle"></i>
+                                    <span>Profile</span>
+                                </a>
+                                <a href="#" class="dropdown-item">
+																		<i class="bi bi-sign-turn-left-fill"></i>
+                                    <span>Logout</span>
+                                </a>       
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <main class="content px-3 py-4">
+                <div class="container-fluid">
+                    <div class="mb-3">
+                    </div>
+                </div>
+            </main>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row text-body-secondary">
+                        <div class="col-6 text-start ">
+                            <a class="text-body-secondary" href=" #">
+                                <strong>Quilana</strong>
+                            </a>
+                        </div>
+                        <div class="col-6 text-end text-body-secondary d-none d-md-block">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <a class="text-body-secondary" href="#">Contact</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="text-body-secondary" href="#">About Us</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="text-body-secondary" href="#">Terms & Conditions</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+<script>
+	const hamBurger = document.querySelector(".toggle-btn");
+	hamBurger.addEventListener("click", function () 
+	{
+  document.querySelector("#sidebar").classList.toggle("expand");
+	});
+</script>
+
+
+<!-- 
+<nav class = "navbar navbar-header navbar-light bg-primary">
 			<div class = "container-fluid">
 				<div class = "navbar-header">
 					<p class = "navbar-text pull-right"><h3 style="color:white">Quilana</h3></p>
@@ -45,7 +150,7 @@
 
 		<?php endif; ?>
 
-		</div>
+		</div> -->
 		<script>
 			$(document).ready(function(){
 				var loc = window.location.href;
