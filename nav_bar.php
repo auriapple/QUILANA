0a7a2cf77f8a80945e3c7b977a80d83efebe9eef
Kath <katherinedmagno@iskolarngbayan.pup.edu.ph>
2024-09-05 06:16:03 -0700

    updated landing/signin/signup/navbar php and css

--- nav_bar.php
+++ nav_bar.php
@@ -10,7 +10,7 @@
 		</nav>
 		<style>
             .bg-primary{
-                background-color: #1E1A43!important;
+                background-image: linear-gradient(to right, #1E1A43, #4A4CA6 85%) !important;
             }
         </style>
 		<div id="sidebar" class="bg-light">
