644ae09091b290ebe0a80df89495a003c9df27d5
Irish Tegio <irishbtegio@gmail.com>
2024-09-16 00:48:42 +0800

    styling changes

--- enroll.php
+++ enroll.php
@@ -33,7 +33,7 @@
                 $student_id = $_SESSION['login_id'];
 
                 // Fetch student's enrolled classes
-                $enrolled_classes_query = $conn->query("SELECT c.class_id, c.subject, f.firstname, f.lastname 
+                $enrolled_classes_query = $conn->query("SELECT c.class_id, c.subject, c.class_name, f.firstname, f.lastname 
                                                         FROM student_enrollment e
                                                         JOIN class c ON e.class_id = c.class_id
                                                         JOIN faculty f ON c.faculty_id = f.faculty_id
@@ -45,7 +45,7 @@
                 <!-- Display class details -->
                 <div class="class-card">
                     <div class="class-card-title"><?php echo $row['subject'] ?></div>
-                    <div class="class-card-text">Professor: <?php echo $row['firstname'] . ' ' . $row['lastname'] ?></div>
+                    <div class="class-card-text">Section: <?php echo $row['class_name'] ?> <br>Professor: <?php echo $row['firstname'] . ' ' . $row['lastname'] ?></div>
                     <div class="class-actions">
                         <button id="viewClassDetails_<?php echo $row['class_id']; ?>" class="main-button" data-id="<?php echo $row['class_id'] ?>" type="button">View Class</button>
                     </div>
