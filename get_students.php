fbb934503434983a14a48e4020786ba633c52ede
kath <katherinedmagno@iskolarngbayan.pup.edu.ph>
2024-11-06 02:31:01 -0800

    quick fixes

--- get_students.php
+++ get_students.php
@@ -4,9 +4,10 @@ include('db_connect.php');
 $class_id = $_POST['class_id'];
 
 $qry_student = $conn->query("
-    SELECT s.student_id, s.student_number, CONCAT(s.lastname, ', ', s.firstname) AS student_name, se.status
+    SELECT s.student_id, s.student_number, CONCAT(s.lastname, ', ', s.firstname) AS student_name, c.class_name, c.subject, se.status
     FROM student_enrollment se
     JOIN student s ON se.student_id = s.student_id
+    JOIN class c ON se.class_id = c.class_id
     WHERE se.class_id = '$class_id' AND se.status != 2
     ORDER BY se.status ASC, student_name ASC
 ");
@@ -25,12 +26,14 @@ if (isset($qry_student) && $qry_student->num_rows > 0) {
                             data-student-id="' . $student['student_id'] . '" 
                             data-status="1" 
                             data-student-name="' . $student['student_name'] . '"
+                            data-class-sub="' . $student['class_name'] . ' (' . $student['subject'] . ')"
                             type="button">Accept</button>
                     <button class="btn btn-danger btn-sm reject-btn" 
                             data-class-id="' . $class_id . '" 
                             data-student-id="' . $student['student_id'] . '" 
                             data-status="2" 
                             data-student-name="' . $student['student_name'] . '"
+                            data-class-sub="' . $student['class_name'] . ' (' . $student['subject'] . ')"
                             type="button">Reject</button>
                 </div>';
         } else {
